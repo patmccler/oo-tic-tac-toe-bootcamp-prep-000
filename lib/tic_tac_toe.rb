5f47920e11b6d457148784f3df06f4baa54f0a06
Patrick McClernan <patmccler@gmail.com>
2017-10-23 23:41:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,7 +5,27 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
+  WIN_COMBINATIONS = [
+    [0,1,2], #top row
+    [3,4,5], #mid row
+    [6,7,8], #bot row
+    [0,3,6], #left col
+    [1,4,7], #mid col
+    [2,4,8], #bot col
+    ]
 
+  def display_board
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+  def input_to_index(input)
+    input = input.to_i
+    input.between?(1,9) ? input - 1 : -1
+  end
 
 
 
