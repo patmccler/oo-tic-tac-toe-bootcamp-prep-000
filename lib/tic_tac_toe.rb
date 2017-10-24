dbbc4aa70fd10042bc32ab86ac83853112e8b5ab
Patrick McClernan <patmccler@gmail.com>
2017-10-24 00:05:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,4 +35,35 @@ class TicTacToe
     !position_taken?(index) && index.between?(0,8)
   end
 
+  def turn_count
+    used_squares = @board.reject do |position|
+      position == "" || position == " " || position.nil?
+    end
+    turn_count.size
+  end
+
+  def current_player
+    turn_count % 2 == 1 ? "X" : "0"
+  end
+
+  def turn
+      until good_move
+      puts "Choose a move by entering 1-9"
+      index = input_to_index(gets.strip)
+      good_move = valid_move(index)
+    end    
+      move(index, current_player)
+      display_board
+  end
+
+  def move(index, element)
+    @board[index] = element
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |win_con|
+      @board[win_con[0]] == @board[win_con[0] && @board[win_con[0]]== @board[win_con[0]] && position_taken?(@board[win_con[0]])
+    end
+  end
+
 end
