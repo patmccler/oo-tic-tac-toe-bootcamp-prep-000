24e768c8397ebaddc12f50ac6d847ee43e3ed5d0
Patrick McClernan <patmccler@gmail.com>
2017-10-24 16:10:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,7 @@ class TicTacToe
   end
 
   def position_taken?(index)
-    !(board[index] == " " || board[index] == "" || board[index].nil?)
+    !(@board[index] == " " || @board[index] == "" || @board[index].nil?)
   end
 
   def valid_move?(index)
